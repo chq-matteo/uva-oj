1e5a950417a33ade47ee9b5871b1bda22b9db639
Matteo C. Q <chq.matteo@gmail.com>
2017-12-31 18:04:20 +0100

    Add 264 Count on Cantor

---
+++ src/swerc2018/264.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long n;
+    while (cin >> n) {
+        // k * (k + 1) / 2 <= n <= (k + 1)(k + 2) / 2
+        // k * (k + 1) <= 2n <= (k + 1)(k + 2)
+        // k^2 + k <= 2n <= k^2 + 2k + 1 + k + 1
+        // we are left to compute such k
+        // well actually binary search works as well
+        long long h, l, m;
+        h = 2 * n;
+        l = 0;
+        while (h - l > 1) {
+            m = l + (h - l) / 2;
+            if (m * m + m < 2*n) {
+                l = m;
+            }
+            else {
+                h = m;
+            }
+        }
+        
+        long long d = n - l * (l +  1) / 2;
+        l++;
+        // now is it going up or down the diagonal
+        if (l % 2 == 0) {
+            // its going down
+            printf("TERM %lld IS %lld/%lld\n", n, d, l - d + 1);
+        }
+        else {
+            printf("TERM %lld IS %lld/%lld\n", n, l - d + 1, d);
+        }
+    }
+}
